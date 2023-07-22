1bf7242d11b322ca59c7e3f68d7fccb6381114dd
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-07-22 17:34:54 -0400

    Create middleware for logging requests to the server

--- backend/util/config.js
+++ backend/util/config.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 
-//Different Database URI for different environments to make viewing test data simpler
+// setup different
 const MONGODB_URI =
   process.env === "test"
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI;
 
-const PORT = process.env.PORT;
+const { PORT } = process.env;
 
 module.exports = { MONGODB_URI, PORT };
