0772bd5990686cc537fd9971809b398ebed3c926
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-07-22 08:12:21 -0400

    feat: Setup development workflow for frontend

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "core-js/stable/index";
+import "regenerator-runtime/runtime";
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
---
+++ client/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+//Simplify creating an HTML file for webpack
+const HtmlWebpackplugin = require("html-webpack-plugin");
+const config = () => {
+  return {
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "main.js",
+    },
+    devServer: {
+      static: path.resolve(__dirname, "build"),
+      compress: true,
+      port: 3000,
+    },
+    plugins: [
+      new HtmlWebpackplugin({
+        template: "./src/index.html",
+        filename: "index.html",
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+          options: { presets: ["@babel/preset-react", "@babel/preset-env"] },
+        },
+      ],
+    },
+    devtool: "source-map",
+  };
+};
+
+module.exports = config;
