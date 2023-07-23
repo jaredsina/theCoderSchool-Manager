06ff1d1a61cf6f165c01fb053ceb1ddb6a14b33e
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-07-23 10:22:03 -0400

    Fix the setup of the test database

--- backend/util/config.js
+++ backend/util/config.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 
-// setup different
+// setup different database for testing
 const MONGODB_URI =
-  process.env === "test"
+  process.env.NODE_ENV === "test"
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI;
 
+// hide port
 const { PORT } = process.env;
 
 module.exports = { MONGODB_URI, PORT };
