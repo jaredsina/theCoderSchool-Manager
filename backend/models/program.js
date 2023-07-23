1ddbfec6886b870c868e6f56607533ef929ad11e
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-07-23 10:30:43 -0400

    feat(programs): Add Program mongoose model
    This commit adds a new model called Program that uses a schema with requirements
    
    This provides a way to set the schema of our database when creating new programs on the app

---
+++ backend/models/program.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const programSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  students: { type: Number, required: true },
+  weeks: { type: Number }, // number of weeks the program is on for
+  description: { type: String },
+  classes: { type: Number }, // number of classes the program has
+  status: { type: Boolean, required: true, default: false },
+  pricing: { type: Number, required: true },
+  invoice: { type: String },
+  staff: {},
+  files: {},
+});
+
+programSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    const updatedObject = returnedObject;
+    updatedObject.id = returnedObject._id.toString();
+    delete updatedObject._id;
+    delete updatedObject.__v;
+    return updatedObject;
+  },
+});
+
+const Program = mongoose.model("Program", programSchema);
+
+module.exports = Program;
