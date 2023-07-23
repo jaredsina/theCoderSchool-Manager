8fba07fe4a8bc4381f006d21c143aa38b5e0f748
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-07-23 10:42:56 -0400

    feat: Adds new middleware and minor changes to express app

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,38 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
-//setup port and mongodb uri for the app
+// no try/catch blocks for aync/await
+require("express-async-errors");
+
+// setup port and mongodb uri for the app
 const config = require("./util/config");
 const logger = require("./util/logger");
+const requestLogger = require("./middleware/requestLogger");
+const programRouter = require("./routers/programRouter");
+const errorHandler = require("./middleware/errorHandler");
+
 const app = express();
 
-app.listen(config.PORT, () => {
+// setup connection to mongodb
+logger.info("Trying to connect to database");
+mongoose.connect(config.MONGODB_URI);
+
+// parse json data in request body and convert to js object
+app.use(express.json());
+
+// log requests made to the app
+app.use(requestLogger);
+
+app.use("/api/programs", programRouter);
+
+app.use(errorHandler);
+
+// dont listen for requests until routes and middleware are set
+const server = app.listen(config.PORT, () => {
   logger.info(`Server running on ${config.PORT}`);
 });
+
+if (process.env.NODE_ENV === "test") {
+  // export server and app for testing
+  module.exports = { app, server };
+}
