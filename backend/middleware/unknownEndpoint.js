c9f1ea9850f9929a57056daf829e306d7fadb4fc
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-07-24 10:51:27 -0400

    Create middleware for catching requests made to non-existent routes

---
+++ backend/middleware/unknownEndpoint.js
@@ -0,0 +1,5 @@
+// return proper message an unknown endpoint is reached
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+module.exports = unknownEndpoint;
