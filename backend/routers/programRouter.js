29eb3793468e071cc45b32d7e3b8aded571c60e0
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-07-24 10:54:11 -0400

    Add a route for users trying to replace a document

--- backend/routers/programRouter.js
+++ backend/routers/programRouter.js
@@ -3,4 +3,5 @@ const programController = require("../controllers/programController");
 
 programRouter.get("/", programController.getAll);
 programRouter.post("/", programController.postNew);
+programRouter.put("/:id", programController.replace);
 module.exports = programRouter;
