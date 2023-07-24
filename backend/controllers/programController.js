8b3bdbb2145ee3cf4297cc8a04224324851571f1
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-07-24 10:52:34 -0400

    Create controller for replacing an existing document in collection

--- backend/controllers/programController.js
+++ backend/controllers/programController.js
@@ -1,13 +1,36 @@
 const Program = require("../models/program");
 
+// fetches all resources in the collection
 const getAll = async (request, response) => {
   const programs = await Program.find({});
   response.status(200).json(programs);
 };
 
+// creates a new resource based on the request data
 const postNew = async (request, response) => {
   const newProgram = request.body;
   const addedProgram = await Program.create(newProgram);
   response.status(200).json(addedProgram);
 };
-module.exports = { getAll, postNew };
+// replaces the entire identified resource with the request data
+const replace = async (request, response) => {
+  const newProgram = request.body;
+  const oldProgram = await Program.findById(newProgram.id);
+  if (oldProgram) {
+    const addedProgram = await Program.findOneAndReplace(
+      { _id: newProgram.id },
+      newProgram,
+      {
+        new: true,
+      }
+    );
+    response.status(200).json(addedProgram);
+  }
+  if (!oldProgram) {
+    response.status(404).json({ error: "Program was not found" });
+    const error = new Error("Program does not exist");
+    error.status = 401;
+    throw error;
+  }
+};
+module.exports = { getAll, postNew, replace };
