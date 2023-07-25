75e1a63007c51135a1a92fccc65d124dd1c700a2
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-07-25 09:28:49 -0400

    Add controller functions for deleting and updating a resource

--- backend/controllers/programController.js
+++ backend/controllers/programController.js
@@ -1,5 +1,4 @@
 const Program = require("../models/program");
-
 // fetches all resources in the collection
 const getAll = async (request, response) => {
   const programs = await Program.find({});
@@ -22,15 +21,52 @@ const replace = async (request, response) => {
       newProgram,
       {
         new: true,
-      }
+      },
     );
     response.status(200).json(addedProgram);
   }
   if (!oldProgram) {
     response.status(404).json({ error: "Program was not found" });
     const error = new Error("Program does not exist");
-    error.status = 401;
     throw error;
   }
 };
-module.exports = { getAll, postNew, replace };
+// let's you update a resource with the request data
+const update = async (request, response) => {
+  const newProgram = request.body;
+  const oldProgram = await Program.findById(newProgram.id);
+  if (oldProgram) {
+    const addedProgram = await Program.findOneAndUpdate(
+      { _id: newProgram.id },
+      newProgram,
+      {
+        new: true,
+      },
+    );
+    response.status(200).json(addedProgram);
+  }
+  if (!oldProgram) {
+    response.status(404).json({ error: "Program was not found" });
+    const error = new Error("Program does not exist");
+    error.status = 404;
+    throw error;
+  }
+};
+
+// deletes a resource
+const deleteProgram = async (request, response) => {
+  const { id } = request.params;
+  const program = await Program.findById(id);
+  if (program) {
+    const deletedProgram = await Program.findByIdAndRemove(id);
+    response.status(200).json(deletedProgram);
+  }
+  if (!program) {
+    response.status(404).json({ error: "Program was not found" });
+    const error = new Error("Program does not exist");
+    error.status = 404;
+    throw error;
+  }
+};
+
+module.exports = { getAll, postNew, replace, update, deleteProgram };
