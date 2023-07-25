4f3dfb9636c8906e5778bd2b4d74b743d7be1c72
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-07-25 09:45:46 -0400

    Create routes to the update and delete controllers

--- backend/routers/programRouter.js
+++ backend/routers/programRouter.js
@@ -1,7 +1,18 @@
 const programRouter = require("express").Router();
 const programController = require("../controllers/programController");
 
+// get all programs
 programRouter.get("/", programController.getAll);
+
+// post a new program
 programRouter.post("/", programController.postNew);
+
+// replace a program
 programRouter.put("/:id", programController.replace);
+
+// update a program
+programRouter.patch("/:id", programController.update);
+
+// delete a program
+programRouter.delete("/:id", programController.deleteProgram);
 module.exports = programRouter;
