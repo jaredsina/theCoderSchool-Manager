4a0a35e34f8ba4d085cf164aad68511e7bdc69c4
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-07-25 09:47:21 -0400

    Group tests for each route

--- backend/tests/program_api.test.js
+++ backend/tests/program_api.test.js
@@ -16,55 +16,117 @@ beforeEach(async () => {
   const uploadedUser = await Program.create(testDataList[0]);
 });
 
-test("should return a program", async () => {
-  const response = await api
-    .get("/api/programs/")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-  expect(response.body).toHaveLength(1);
+describe("get route", () => {
+  test("should return a program", async () => {
+    const response = await api
+      .get("/api/programs/")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+    expect(response.body).toHaveLength(1);
+  });
+  test("should return an error with incorrect endpoint", async () => {
+    const response = await api
+      .get("/api/program/")
+      .expect(404)
+      .expect("Content-Type", /application\/json/);
+    expect(response.body).toHaveProperty("error", "unknown endpoint");
+  });
 });
 
-test("should be able to create a new program", async () => {
-  const response = await api
-    .post("/api/programs/")
-    .send(testDataList[1])
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-  expect(response.body.name).toContain("Science Program");
-  const data = await dataInDb();
-  expect(data).toHaveLength(2);
+describe("put route", () => {
+  test("should replace a program correctly", async () => {
+    const programsAtStart = await dataInDb();
+    const oldProgram = programsAtStart.find(
+      (program) => program.name === "Math Program",
+    );
+    const oldProgramId = oldProgram.id;
+    const response = await api
+      .put(`/api/programs/${oldProgramId}`)
+      .send({ ...oldProgram, name: "SCAP" })
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+    const programs = await dataInDb();
+    expect(programs).toHaveLength(1);
+    expect(programs[0]).toHaveProperty("name", "SCAP");
+  });
+  test("should return error when trying to replace a program that doesnt exist", async () => {
+    const response = await api
+      .put("/api/programs/dsfsdfs")
+      .send({
+        name: "joe",
+      })
+      .expect(404)
+      .expect("Content-Type", /application\/json/);
+  });
 });
-test("should return an error when not putting in required fields of a program", async () => {
-  const response = await api
-    .post("/api/programs/")
-    .send({ ...testDataList[0], name: "" })
-    .expect(400)
-    .expect("Content-Type", /application\/json/);
+
+describe("post route", () => {
+  test("should be able to create a new program", async () => {
+    const response = await api
+      .post("/api/programs/")
+      .send(testDataList[1])
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+    expect(response.body.name).toContain("Science Program");
+    const data = await dataInDb();
+    expect(data).toHaveLength(2);
+  });
+  test("should return an error when not putting in required fields of a program", async () => {
+    const response = await api
+      .post("/api/programs/")
+      .send({ ...testDataList[0], name: "" })
+      .expect(400)
+      .expect("Content-Type", /application\/json/);
+  });
 });
-test("should replace a program correctly", async () => {
-  const programsAtStart = await dataInDb();
-  const oldProgram = programsAtStart.find(
-    (program) => program.name === "Math Program"
-  );
-  const oldProgramId = oldProgram.id;
-  const response = await api
-    .put(`/api/programs/${oldProgramId}`)
-    .send({ ...oldProgram, name: "SCAP" })
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-  const programs = await dataInDb();
-  expect(programs).toHaveLength(1);
-  expect(programs[0]).toHaveProperty("name", "SCAP");
+
+describe("patch route", () => {
+  test("should update a program correctly", async () => {
+    const programsAtStart = await dataInDb();
+    const oldProgram = programsAtStart.find(
+      (program) => program.name === "Math Program",
+    );
+    const oldProgramId = oldProgram.id;
+    const response = await api
+      .patch(`/api/programs/${oldProgramId}`)
+      .send({ ...oldProgram, name: "SCAP" })
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+    const programs = await dataInDb();
+    expect(programs).toHaveLength(1);
+    expect(programs[0]).toHaveProperty("name", "SCAP");
+  });
+  test("should return error when trying to update a program that doesnt exist", async () => {
+    const response = await api
+      .patch("/api/programs/dsfsdfs")
+      .send({ name: "joe" })
+      .expect(404)
+      .expect("Content-Type", /application\/json/);
+  });
 });
-test("should return error when trying to replace a program that doesnt exist", async () => {
-  const response = await api
-    .put("/api/programs/dsfsdfs")
-    .send({
-      name: "joe",
-    })
-    .expect(404)
-    .expect("Content-Type", /application\/json/);
+
+describe("delete route", () => {
+  test("should delete a program correctly", async () => {
+    const programsAtStart = await dataInDb();
+    const oldProgram = programsAtStart.find(
+      (program) => program.name === "Math Program",
+    );
+    const oldProgramId = oldProgram.id;
+    const response = await api
+      .delete(`/api/programs/${oldProgramId}`)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+    const programs = await dataInDb();
+    expect(programs).toHaveLength(0);
+  });
+  test("should return error when trying to delete a program that doesnt exist", async () => {
+    const response = await api
+      .delete("/api/programs/64bd2e5ee9849c40cdb84199")
+      .expect(404)
+      .expect("Content-Type", /application\/json/);
+  });
 });
+
 afterAll(async () => {
   //remove any open handles so jest will shutdown properly
   await mongoose.connection.close();
