f2aa1479fca8ee50f2cae7b23968d567ca311fa5
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-07-26 14:25:39 -0400

    Configure eslint and prettier for developing the frontend

---
+++ client/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "airbnb",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:prettier/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  plugins: ["react", "jsx-a11y", "import", "prettier", "react-hooks"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
+  },
+};
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,37 +1,36 @@
 const path = require("path");
 
-//Simplify creating an HTML file for webpack
+// Simplify creating an HTML file for webpack
 const HtmlWebpackplugin = require("html-webpack-plugin");
-const config = () => {
-  return {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-      path: path.resolve(__dirname, "build"),
-      filename: "main.js",
-    },
-    devServer: {
-      static: path.resolve(__dirname, "build"),
-      compress: true,
-      port: 3000,
-    },
-    plugins: [
-      new HtmlWebpackplugin({
-        template: "./src/index.html",
-        filename: "index.html",
-      }),
+
+const config = () => ({
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "main.js",
+  },
+  devServer: {
+    static: path.resolve(__dirname, "build"),
+    compress: true,
+    port: 3001,
+  },
+  plugins: [
+    new HtmlWebpackplugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/preset-react", "@babel/preset-env"] },
+      },
     ],
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          loader: "babel-loader",
-          options: { presets: ["@babel/preset-react", "@babel/preset-env"] },
-        },
-      ],
-    },
-    devtool: "source-map",
-  };
-};
+  },
+  devtool: "source-map",
+});
 
 module.exports = config;
