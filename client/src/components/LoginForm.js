3273cdd81f9c950088d2401396446286904f5db1
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-07-28 11:35:47 -0400

    Add id and name for easier testing

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -6,18 +6,28 @@ import Notification from "./Notification";
 const LoginForm = () => {
   // dispatch is a function that takes an action as an argument
   const dispatch = useDispatch();
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    const username = event.target[0].value;
-    const password = event.target[1].value;
+    const username = event.target.username.value;
+    const password = event.target.password.value;
     dispatch(attemptLogin(username, password));
   };
   return (
     <div>
       <h1>Login Form</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" />
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          name="username"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          name="password"
+        />
         <button type="submit">Login</button>
       </form>
       <Notification />
