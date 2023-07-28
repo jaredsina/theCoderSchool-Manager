1b06f21cbcbd0ec8fee168aeb66bb0ca7b4e897f
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-07-28 12:00:07 -0400

    Create a test util that helps setup testing react components that use redux

---
+++ client/src/utils/test-utils.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import authReducer from "../reducers/authReducer";
+import notificationReducer from "../reducers/notificationReducer";
+
+export function renderWithProviders(
+  ui,
+  {
+    preloadedState = {},
+    // Automatically create a store instance if no store was passed in
+    store = configureStore({
+      reducer: { user: authReducer, notification: notificationReducer },
+      preloadedState,
+    }),
+    ...renderOptions
+  } = {},
+) {
+  const Wrapper = ({ children }) => {
+    return <Provider store={store}>{children}</Provider>;
+  };
+
+  // Return an object with the store and all of RTL's query functions
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+}
