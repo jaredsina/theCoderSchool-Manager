5e6ca8a5a7d88647f9d873a353a459fde2977c03
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-07-29 14:00:19 -0400

    Create actions for checking if a user is logged in or is logging out

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -27,4 +27,17 @@ export const attemptLogin = (username, password) => async (dispatch) => {
   }
 };
 
+export const logout = () => async (dispatch) => {
+  window.localStorage.removeItem("user");
+  dispatch(setUser(null));
+};
+
+export const initializeUser = () => async (dispatch) => {
+  const loggedUserJSON = window.localStorage.getItem("user");
+  if (loggedUserJSON) {
+    const user = JSON.parse(loggedUserJSON);
+    dispatch(setUser(user));
+  }
+};
+
 export default authSlice.reducer;
