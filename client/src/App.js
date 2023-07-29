2d51aa1865aa92e759c1dd11b678665e6da18c8d
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-07-29 13:59:06 -0400

    Use a Router to route logged in users to the Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import LoginForm from "./components/LoginForm";
+import Dashboard from "./components/Dashboard";
+import { initializeUser } from "./reducers/authReducer";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  // when the app is first rendered, we want to check if the user is logged in
+  useEffect(() => {
+    dispatch(initializeUser());
+  }, [dispatch]);
+
+  const user = useSelector((state) => state.user);
   return (
     <div className="App">
-      <LoginForm />
+      <Routes>
+        <Route path="/" element={user ? <Dashboard /> : <LoginForm />} />
+      </Routes>
     </div>
   );
 };
