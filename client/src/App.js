da1079c97141d583175f571f55af9048a07e4977
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-07-31 10:59:24 -0400

    Add a program initializer to prepare programs and check token

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,19 @@ import { useSelector, useDispatch } from "react-redux";
 import LoginForm from "./components/LoginForm";
 import Dashboard from "./components/Dashboard";
 import { initializeUser } from "./reducers/authReducer";
+import { initializePrograms } from "./reducers/programsReducer";
 
 const App = () => {
   const dispatch = useDispatch();
-
+  const user = useSelector((state) => state.user);
   // when the app is first rendered, we want to check if the user is logged in
   useEffect(() => {
     dispatch(initializeUser());
+    if (user) {
+      dispatch(initializePrograms());
+    }
   }, [dispatch]);
 
-  const user = useSelector((state) => state.user);
   return (
     <div className="App">
       <Routes>
