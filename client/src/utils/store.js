983e46f2aead1f50078e29a739c93d667cdf1dd8
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-07-31 10:08:52 -0400

    Add a state for programs and a reducer for it

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from "../reducers/authReducer";
 import notificationReducer from "../reducers/notificationReducer";
+import programsReducer from "../reducers/programsReducer";
 
 const store = configureStore({
   reducer: {
     // reducers go here
     user: authReducer,
     notification: notificationReducer,
+    programs: programsReducer,
   },
 });
 
