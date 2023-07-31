81c4d3c274ca30cfb000cdbfdab692e6185efa9e
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-07-31 11:02:20 -0400

    Create a token and config to send valid requests to the backend

--- client/src/services/program.js
+++ client/src/services/program.js
@@ -2,11 +2,21 @@ import axios from "axios";
 
 const baseUrl = "http://localhost:3000/api/programs";
 
+let token = null;
+let config = {};
+
+const setToken = (newToken) => {
+  token = `bearer ${newToken}`;
+  config = {
+    headers: { Authorization: token },
+  };
+};
+
 // The getAll function sends a GET request to the /api/programs endpoint.
 // If the request is successful, the function returns the response data.
 const getAll = async () => {
-  const response = await axios.get(baseUrl);
+  const response = await axios.get(baseUrl, config);
   return response.data;
 };
 
-export default { getAll };
+export default { getAll, setToken };
