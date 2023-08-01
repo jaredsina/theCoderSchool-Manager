dc354dad22efd97a5d353d6c36e9f5d6dbfb64cc
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-08-01 08:07:37 -0400

    Create action creater for adding new programs to the state and database

--- client/src/reducers/programsReducer.js
+++ client/src/reducers/programsReducer.js
@@ -10,10 +10,13 @@ const programsSlice = createSlice({
     setPrograms: (state, action) => {
       return action.payload;
     },
+    appendProgram: (state, action) => {
+      return [...state, action.payload];
+    },
   },
 });
 
-export const { setPrograms } = programsSlice.actions;
+export const { setPrograms, appendProgram } = programsSlice.actions;
 
 export default programsSlice.reducer;
 
@@ -25,5 +28,15 @@ export const initializePrograms = () => async (dispatch) => {
     // if the token has expired, log the user out
     dispatch(logout());
     dispatch(displayMessage("Session expired, please login again", "error", 5));
+  }
+};
+
+export const addProgram = (program) => async (dispatch) => {
+  try {
+    const newProgram = await ProgramService.create(program);
+    dispatch(displayMessage(`Added ${newProgram.name}`, "success", 5));
+    dispatch(appendProgram(newProgram));
+  } catch (err) {
+    dispatch(displayMessage("Error adding program", "error", 5));
   }
 };
