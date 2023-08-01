e3dfccc4048b18e37bed728757a8c116a5985f04
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-08-01 08:42:06 -0400

    Add new components to the dashboard view

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import Sidebar from "./Sidebar";
 import { logout } from "../reducers/authReducer";
+import ProgramForm from "./ProgramForm";
+import Program from "./Program";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
@@ -10,10 +12,11 @@ const Dashboard = () => {
       <button type="button" onClick={() => dispatch(logout())}>
         Logout
       </button>
+      <ProgramForm />
       <h1>Dashboard</h1>
       <Sidebar />
       <div className="main">
-        <h2>Program</h2>
+        <Program />
       </div>
     </div>
   );
