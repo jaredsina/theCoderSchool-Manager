f6decf87b776e33bb9c13479b0d637a3bbdf7227
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-08-02 08:40:22 -0400

    Create routes for dashboard and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import LoginForm from "./components/LoginForm";
 import Dashboard from "./components/Dashboard";
@@ -9,6 +9,7 @@ import { initializePrograms } from "./reducers/programsReducer";
 const App = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
+
   // when the app is first rendered, we want to check if the user is logged in
   useEffect(() => {
     dispatch(initializeUser());
@@ -18,7 +19,18 @@ const App = () => {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={user ? <Dashboard /> : <LoginForm />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route
+          path="/"
+          element={
+            user ? (
+              <Navigate replace to="/dashboard" />
+            ) : (
+              <Navigate replace to="/login" />
+            )
+          }
+        />
       </Routes>
     </div>
   );
