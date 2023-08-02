21a54b5d56c39016fbf91d800ad0ae97c0e5b691
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-08-02 08:41:34 -0400

    Add navigation to dashboard if user is logged in

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,17 +1,30 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { attemptLogin } from "../reducers/authReducer";
 import Notification from "./Notification";
 
 const LoginForm = () => {
   // dispatch is a function that takes an action as an argument
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
   const handleSubmit = async (event) => {
     event.preventDefault();
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
-    dispatch(attemptLogin(username, password));
+    await dispatch(attemptLogin(username, password));
+    if (user) {
+      navigate("/dashboard");
+    }
   };
+  // when the LoginForm mounts lets check if user exists the store and if so redirect to dashboard
+  useEffect(() => {
+    if (user) {
+      navigate("/dashboard");
+    }
+  }, [user, navigate]);
+
   return (
     <div>
       <h1>Login Form</h1>
