8d05926252bbd25ecc1cfe86f5ab78282299ae6e
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-08-02 10:12:48 -0400

    Add program info to displayed program

--- client/src/components/Program.js
+++ client/src/components/Program.js
@@ -7,15 +7,25 @@ const Program = () => {
   const programs = useSelector((state) => state.programs);
 
   const program = match ? programs.find((p) => p.id === match.params.id) : null;
-  console.log(program);
-
   if (!program) {
     return null;
   }
-
   return (
     <div>
       <h2>Program</h2>
+      <h3>{program.name}</h3>
+      <p>{program.status ? "Active: True" : "Active: False"}</p>
+      <p>{program.description}</p>
+      <p>Invoice due date: {program.invoice}</p>
+      <p>Number of classes: {program.classes}</p>
+      <p>Number of students: {program.students}</p>
+      <p>Price Per Student: {program.pricing}</p>
+      <p>Price Per month: {program.pricing * program.students}</p>
+      <p>Program Total: {program.pricing * program.students * program.weeks}</p>
+      <p>Number of weeks in the program: {program.weeks}</p>
+      <p>Assigned Staff: {program.staff}</p>
+      <p>Task Alerts: </p>
+      <p>Files: </p>
     </div>
   );
 };
