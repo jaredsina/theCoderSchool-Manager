4d5fc00d0a7b0127c8e2a148765756389b2908e7
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-08-02 10:11:40 -0400

    Create a logout button and a checker for if the user is not logged in

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import { logout } from "../reducers/authReducer";
 import ProgramForm from "./ProgramForm";
@@ -7,9 +8,23 @@ import Program from "./Program";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const logoutHandler = () => {
+    dispatch(logout());
+    navigate("/login");
+  };
+  // when the Dashboard mounts lets check if user exists the store and if not redirect to login
+  useEffect(() => {
+    const user = JSON.parse(window.localStorage.getItem("user"));
+    if (!user) {
+      navigate("/login");
+    }
+  }, [navigate]);
+
   return (
     <div>
-      <button type="button" onClick={() => dispatch(logout())}>
+      <button type="button" onClick={logoutHandler}>
         Logout
       </button>
       <ProgramForm />
