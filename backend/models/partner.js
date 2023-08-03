aaf30cc68c3c09f7148718ba211c38c62a734d5a
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-08-03 10:33:07 -0400

    Create partner model

---
+++ backend/models/partner.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const partnerSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  schoolname: { type: String },
+  website: { type: String },
+  email: { type: String },
+  phone: { type: String },
+  address: { type: String },
+  primarycontact: { type: String },
+  primarycontactemail: { type: String },
+  primarycontactphone: { type: String },
+});
+
+partnerSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    const updatedObject = returnedObject;
+    updatedObject.id = returnedObject._id.toString();
+    delete updatedObject._id;
+    delete updatedObject.__v;
+    return updatedObject;
+  },
+});
+
+const Partner = mongoose.model("Partner", partnerSchema);
+
+module.exports = Partner;
