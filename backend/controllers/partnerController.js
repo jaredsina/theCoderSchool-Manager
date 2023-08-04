794fb3b3dec84c28d8c848a33aa1351577ffaeac
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-08-04 09:33:04 -0400

    Populate each partners program list with any existing connections to other documents

--- backend/controllers/partnerController.js
+++ backend/controllers/partnerController.js
@@ -1,7 +1,9 @@
 const Partner = require("../models/partner");
 
 const getPartners = async (request, response) => {
-  const partners = await Partner.find({});
+  const partners = await Partner.find({}).populate("programs", {
+    partner: 0,
+  });
   response.status(200).json(partners);
 };
 
