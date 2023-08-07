f1276e2f3561e94482eaf5949f0794a502d12a9e
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-08-07 12:47:26 -0400

    Fix Dashboard bugs and add new Partner functionality
    Dashboard would not update the partners and programs after logging in. Setting a useEffect to run on mount fixed the problem of them only being called by the App. Now when the user logs out and back in without reloading, it can properly fetch data from the database for the dashboards components.
    
    Dashboard now dispatches calls to remove the partners and programs from state on a logout button click.
    
    PartnerForm is now displayed on the dashboard with a view being available if the user clicks on a specific partner.

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,39 +1,63 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React, { useDebugValue, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Sidebar from "./Sidebar";
-import { logout } from "../reducers/authReducer";
+import { logout, initializeUser } from "../reducers/authReducer";
 import ProgramForm from "./ProgramForm";
 import Program from "./Program";
 import Notification from "./Notification";
+import PartnerForm from "./PartnerForm";
+import {
+  initializePrograms,
+  removeAllProgramsState,
+} from "../reducers/programsReducer";
+import {
+  initializePartners,
+  removeAllPartnersState,
+} from "../reducers/partnersReducer";
+import Partner from "./Partner";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  // logging out the user and removing the programs and partners from the store
   const logoutHandler = () => {
     dispatch(logout());
+    dispatch(removeAllPartnersState());
+    dispatch(removeAllProgramsState());
     navigate("/login");
   };
-  // when the Dashboard mounts lets check if user exists the store and if not redirect to login
+  // when the Dashboard first mounts lets check if user exists the store and if not redirect to login
   useEffect(() => {
-    const user = JSON.parse(window.localStorage.getItem("user"));
-    if (!user) {
+    const userJSON = JSON.parse(window.localStorage.getItem("user"));
+    if (!userJSON) {
       navigate("/login");
     }
   }, [navigate]);
 
+  // when the app is first rendered, we want to check if the user is logged in
+  // and if so, we want to fetch the programs and partners from the server to validate the token
+
+  useEffect(() => {
+    dispatch(initializeUser());
+    dispatch(initializePrograms());
+    dispatch(initializePartners());
+  }, [dispatch]);
   return (
     <div>
       <button type="button" onClick={logoutHandler}>
         Logout
       </button>
       <ProgramForm />
+      <PartnerForm />
       <Notification />
+
       <h1>Dashboard</h1>
       <Sidebar />
       <div className="main">
         <Program />
+        <Partner />
       </div>
     </div>
   );
