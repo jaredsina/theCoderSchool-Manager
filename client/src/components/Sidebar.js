314755d1f28beb09b55b62098eefd92c4337be42
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-08-07 12:56:16 -0400

    Add partners to the sidebar and organize programs by their partners

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
+  const partners = useSelector((state) => state.partners);
   const programs = useSelector((state) => state.programs);
-  const programList = programs.map((program) => (
-    <li key={program.id}>
-      <Link to={`/dashboard/${program.id}`}>{program.name}</Link>
+
+  // when the programs or partners are updated, the sidebar will be updated
+  useEffect(() => {}, [programs, partners]);
+
+  // get a list of all the partners and their programs
+  const partnersList = partners.map((partner) => (
+    <li key={partner.id}>
+      <Link to={`/dashboard/${partner.id}`}>{partner.name}</Link>
+      <ul>
+        {partner.programs.map((program) => (
+          <li key={program.id}>
+            <Link to={`/dashboard/${program.id}`}>{program.name}</Link>
+          </li>
+        ))}
+      </ul>
     </li>
   ));
-
+  // get a list of all the programs without partners
+  const programsWithoutPartners = programs.map((program) => {
+    if (program.partner === null) {
+      return (
+        <li key={program.id}>
+          <Link to={`/dashboard/${program.id}`}>{program.name}</Link>
+        </li>
+      );
+    }
+    return null;
+  });
   return (
     <div className="sidebar">
-      <h2>Programs</h2>
-      <ul>{programList}</ul>
+      <ul>{partnersList}</ul>
+      No Partner
+      <ul>{programsWithoutPartners}</ul>
     </div>
   );
 };
