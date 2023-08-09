d453cc138cfb95ed4b57f6b86f3ef7a97f12ae31
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-08-09 08:14:03 -0400

    Fix bug where returnedObject._id was null

--- backend/models/program.js
+++ backend/models/program.js
@@ -19,11 +19,10 @@ const programSchema = new mongoose.Schema({
 
 programSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    const updatedObject = returnedObject;
-    updatedObject.id = returnedObject._id.toString();
-    delete updatedObject._id;
-    delete updatedObject.__v;
-    return updatedObject;
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    return returnedObject;
   },
 });
 
