3306a344c259d555a29d3435be48a57f91fd10a4
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-08-13 09:29:12 -0400

    feat: Create service to work with files from the database

---
+++ client/src/services/file.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:3000/api/files";
+
+let token = null;
+let config = {};
+
+const setToken = (newToken) => {
+  token = `bearer ${newToken}`;
+  config = {
+    headers: { Authorization: token },
+  };
+};
+
+// The getAll function sends a GET request to the /api/files endpoint.
+// If the request is successful, the function returns the response data.
+const getAll = async () => {
+  const response = await axios.get(baseUrl, config);
+  return response.data;
+};
+
+// The create function sends a POST request to the /api/files endpoint with the file data.
+// If the request is successful, the function returns the response data.
+const create = async (newFile) => {
+  const response = await axios.post(baseUrl, newFile, config);
+  return response.data;
+};
+
+// The delete function sends a DELETE request to the /api/files/:id endpoint.
+// If the request is successful, the function returns the response data.
+const deleteFile = async (id) => {
+  const response = await axios.delete(`${baseUrl}/${id}`, config);
+  return response.data;
+};
+
+// The update function sends a PUT request to the /api/files/:id endpoint with the file data.
+// If the request is successful, the function returns the response data.
+const getFile = async (id) => {
+  const response = await axios.get(`${baseUrl}/${id}`, config);
+  return response.data;
+};
+
+export default { getAll, setToken, create, deleteFile, getFile };
