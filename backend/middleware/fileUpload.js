50e295c7a7bd73f77705ce8f6b853019ff1ce66b
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-08-17 09:29:44 -0400

    feat: Upload files straight to cloudinary

--- backend/middleware/fileUpload.js
+++ backend/middleware/fileUpload.js
@@ -1,26 +1,29 @@
 const multer = require("multer");
-const File = require("../models/file");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
 
 const upload = multer({
-  storage: multer.diskStorage({
-    // set destination for uploaded files
-    destination: "./uploads",
-    filename: (req, file, cb) => {
-      const filename = file.originalname;
-
-      // check if file already exists in the database
-      const fileModel = File.findOne({ filename: filename });
-
-      if (fileModel) {
-        const [filen, ext] = filename.split(".");
-        // if file exists, append a number to the end of the filename
-        const newFilename = `${filen}-${Date.now()}.${ext}`;
-        cb(null, newFilename);
-      } else {
-        cb(null, filename);
-      }
+  storage: new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+      folder: "files",
+      format: async (request, file) => "png",
+      public_id: (request, file) => file.originalname,
     },
   }),
+  limits: { fileSize: 10 * 1024 * 1024 },
+  fileFilter: (request, file, callback) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif|pdf)$/)) {
+      return callback(new Error("Please upload a valid file"));
+    }
+    callback(undefined, true);
+  },
 });
 
 module.exports = upload;
