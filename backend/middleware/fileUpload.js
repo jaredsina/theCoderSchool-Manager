8d47d72ad8c8f7672657fa260f8b412d7c71ec0b
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-08-18 09:59:59 -0400

    fix: Remove cloudinary because we moved to google cloud storage

--- backend/middleware/fileUpload.js
+++ backend/middleware/fileUpload.js
@@ -1,29 +1,8 @@
 const multer = require("multer");
-const cloudinary = require("cloudinary").v2;
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
-
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
-});
 
 const upload = multer({
-  storage: new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-      folder: "files",
-      format: async (request, file) => "png",
-      public_id: (request, file) => file.originalname,
-    },
-  }),
-  limits: { fileSize: 10 * 1024 * 1024 },
-  fileFilter: (request, file, callback) => {
-    if (!file.originalname.match(/\.(jpg|jpeg|png|gif|pdf)$/)) {
-      return callback(new Error("Please upload a valid file"));
-    }
-    callback(undefined, true);
-  },
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 2 * 1024 * 1024 },
 });
 
 module.exports = upload;
