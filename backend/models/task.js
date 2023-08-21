547fba2665aad8471a8820e8c7dd4659ea97d223
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-08-21 11:14:12 -0400

    feat: Create model for tasks

---
+++ backend/models/task.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  dueDate: {
+    type: Date,
+  },
+  email_alert: {
+    type: Boolean,
+    default: false,
+  },
+  email_sent: {
+    type: Boolean,
+    default: false,
+  },
+  programId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Program",
+  },
+  partnerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Partner",
+  },
+});
+
+taskSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    const updatedObject = returnedObject;
+    updatedObject.id = returnedObject._id.toString();
+    delete updatedObject._id;
+    delete updatedObject.__v;
+    return updatedObject;
+  },
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
