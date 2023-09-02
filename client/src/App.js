0d55ab67740d046aade240c00ca9d4a73de8aa7e
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-09-02 07:32:27 -0400

    fix: Only make requests when user is logged in

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import LoginForm from "./components/LoginForm";
 import Dashboard from "./components/Dashboard";
-import { initializeUser } from "./reducers/authReducer";
-import { initializePrograms } from "./reducers/programsReducer";
-import { initializePartners } from "./reducers/partnersReducer";
-import { initializeTasks } from "./reducers/taskReducer";
 import "./main.css";
 
 const App = () => {
-  const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
-  // when the app is first rendered, we want to check if the user is logged in
-  // and if so, we want to fetch the programs from the server to validate the token
-  useEffect(() => {
-    dispatch(initializeUser());
-    dispatch(initializePrograms());
-    dispatch(initializePartners());
-    dispatch(initializeTasks());
-  }, [dispatch]);
-
   return (
     <div className="App h-screen bg-emerald-950 text-emerald-50 font-sans">
       <Routes>
