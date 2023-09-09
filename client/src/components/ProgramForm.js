4ffac5ca0298b63fc89e277615c40d84f97d3fa1
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-09-08 21:35:47 -0400

    feat: Add styling to program creation form

--- client/src/components/ProgramForm.js
+++ client/src/components/ProgramForm.js
@@ -42,13 +42,15 @@ const ProgramForm = () => {
     dispatch(addProgram(newProgram));
   };
   return (
-    // we are making a form for the program model used on the Backend file program.js
     <div>
-      <h1>Program Form</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="partner">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <label htmlFor="partner" className="flex flex-col gap-2">
           Partner
-          <select id="partner" name="partner">
+          <select
+            id="partner"
+            name="partner"
+            className="bg-gray-50 rounded-lg p-2 border border-gray-300"
+          >
             <option value="" data-key={null}>
               None
             </option>
@@ -63,79 +65,108 @@ const ProgramForm = () => {
             ))}
           </select>
         </label>
-        <label htmlFor="name">
+        <label htmlFor="name" className="flex flex-col gap-2">
           Name*:{" "}
           <input
             type="text"
             placeholder="Name of program"
             id="name"
             name="name"
+            className="rounded-lg p-2 border bg-gray-50 border-gray-300"
             required
           />
         </label>
-        <label htmlFor="status">
-          Active*:
-          <input type="checkbox" id="status" name="status" defaultChecked />
+        <label htmlFor="status" className="flex gap-2">
+          Active:
+          <input
+            type="checkbox"
+            id="status"
+            name="status"
+            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-md border focus:ring-blue-500"
+            defaultChecked
+          />
         </label>
-        <label htmlFor="pricing">
+        <label htmlFor="pricing" className="flex flex-col gap-2">
           Pricing per student*:
           <input
             type="number"
             placeholder="25"
             id="pricing"
             name="pricing"
+            className="rounded-lg p-2 border bg-gray-50 border-gray-300"
             required
           />
         </label>
-        <label htmlFor="students">
+        <label htmlFor="students" className="flex flex-col gap-2">
           Students*:{" "}
           <input
             type="number"
             placeholder="300"
             id="students"
             name="students"
+            className="rounded-lg p-2 border bg-gray-50 border-gray-300"
             required
           />
         </label>
-        <label htmlFor="weeks">
+        <label htmlFor="weeks" className="flex flex-col gap-2">
           Weeks
-          <input type="number" placeholder="4" id="weeks" name="weeks" />
+          <input
+            type="number"
+            placeholder="4"
+            id="weeks"
+            name="weeks"
+            className="rounded-lg p-2 border bg-gray-50 border-gray-300"
+          />
         </label>
-        <label htmlFor="description">
+        <label htmlFor="description" className="flex flex-col gap-2">
           Description
           <input
             type="text"
             placeholder="description"
             id="description"
             name="description"
+            className="rounded-lg p-2 border bg-gray-50 border-gray-300"
           />
         </label>
-        <label htmlFor="classes">
+        <label htmlFor="classes" className="flex flex-col gap-2">
           Classes
           <input
             type="number"
             placeholder="classes"
             id="classes"
             name="classes"
+            className="rounded-lg p-2 border bg-gray-50 border-gray-300"
           />
         </label>
         {/* invoice is a calender due date */}
-        <label htmlFor="invoice">
+        <label htmlFor="invoice" className="flex flex-col gap-2">
           Invoice due date:
-          <input type="date" id="invoice" name="invoice" />
+          <input
+            type="date"
+            id="invoice"
+            name="invoice"
+            className="bg-gray-50 rounded-lg p-2 border border-gray-300 text-black"
+          />
         </label>
-        <label htmlFor="invoicePaid">
+        <label htmlFor="invoicePaid" className="flex gap-2">
           Invoice paid:
           <input
             type="checkbox"
             id="invoicePaid"
             name="invoicePaid"
+            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-md border focus:ring-blue-500"
             defaultChecked
           />
         </label>
-        <label htmlFor="staff">
+        <label htmlFor="staff" className="flex flex-col gap-2">
           Staff
-          <input type="text" placeholder="staff" id="staff" name="staff" />
+          <input
+            type="text"
+            placeholder="staff"
+            id="staff"
+            name="staff"
+            className="rounded-lg p-2 border bg-gray-50 border-gray-300"
+          />
         </label>
         <button type="submit">Submit</button>
       </form>
