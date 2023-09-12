976c3a550757297c7e8e939c8ae8966df4b8f2f9
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-09-12 11:16:28 -0400

    fix: Fix alignment of toggle and create program button

--- client/src/components/Programs.js
+++ client/src/components/Programs.js
@@ -27,32 +27,33 @@ const Programs = () => {
 
   return (
     <>
-      <label className="relative inline-flex items-center cursor-pointer">
-        <input
-          type="checkbox"
-          className="sr-only peer"
-          onClick={() => setStatus(!status)}
-        />
-        <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600" />
-        <span className="ml-3 text-sm font-medium text-emerald-950 dark:text-gray-200">
-          Active Programs
-        </span>
-      </label>
-      <Modal ref={modalRef} header="New Program Form">
-        <ProgramForm />
-      </Modal>
-      <button
-        type="button"
-        className="programCreateButton bg-emerald-950 text-white rounded-lg p-2"
-        onClick={() => modalRef.current.openModal()}
-      >
-        Create Program
-      </button>
+      <div className="flex items-center justify-between">
+        <label className="relative inline-flex items-center cursor-pointer">
+          <input
+            type="checkbox"
+            className="sr-only peer"
+            onClick={() => setStatus(!status)}
+          />
+          <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600" />
+          <span className="ml-4 text-sm font-medium text-emerald-950 dark:text-gray-200">
+            Active Programs
+          </span>
+        </label>
+        <Modal ref={modalRef} header="New Program Form">
+          <ProgramForm closeModal={() => modalRef.current.close()} />
+        </Modal>
+        <button
+          type="button"
+          className="programCreateButton bg-emerald-950 text-white rounded-lg p-2"
+          onClick={() => modalRef.current.openModal()}
+        >
+          Create Program
+        </button>
+      </div>
       <h2 className="font-bold">Programs</h2>
-      <div className="programs-list bg-transparent grid grid-flow-row gap-4">
+      <div className="programs-list p-2 bg-transparent overflow-y-auto overflow-x-hidden grid gap-4 grid-cols-2">
         {programCards}
       </div>
-      <div />
     </>
   );
 };
