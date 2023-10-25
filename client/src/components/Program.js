c7f14c465f39f2212bcbdbcf5664e40e39785b78
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-10-25 13:01:56 -0400

    feat: Organize program info into grid

--- client/src/components/Program.js
+++ client/src/components/Program.js
@@ -89,6 +89,21 @@ const Program = () => {
       </h1>
       <div className=" grid grid-cols-3">
         <div>
+          <h2 className="font-bold text-3xl">Description </h2>
+          {editMode ? (
+            <textarea
+              id="editDescription"
+              style={{ height: 100, width: 300 }}
+              defaultValue={program.description}
+              placeholder="Description of program"
+            />
+          ) : (
+            <h4 className=" bg-emerald-50 px-3 py-2 rounded-lg w-fit">
+              {program.description ? program.description : "None"}
+            </h4>
+          )}
+        </div>
+        <div>
           <h2 className="font-bold text-3xl">Partner </h2>
           {editMode ? (
             <select
@@ -114,122 +129,150 @@ const Program = () => {
             </h4>
           )}
         </div>
+        <div>
+          <h2 className="font-bold text-3xl">Active: </h2>
+          {editMode ? (
+            <input
+              type="checkbox"
+              id="editStatus"
+              defaultChecked={!!program.status}
+            />
+          ) : (
+            <h4 className=" bg-emerald-50 px-3 py-2 rounded-lg w-fit">
+              {program.status.toString()}
+            </h4>
+          )}
+        </div>
+        <div>
+          <h2 className="font-bold text-3xl">Invoice Due Date: </h2>
+          {editMode ? (
+            <input
+              type="date"
+              id="editInvoiceDate"
+              // defaultValue of due date is the programs due date converted from a date object to a string
+              defaultValue={
+                program.invoice
+                  ? new Date(program.invoice).toISOString().split("T")[0]
+                  : ""
+              }
+            />
+          ) : (
+            <h4 className=" bg-emerald-50 px-3 py-2 rounded-lg w-fit">
+              {program.invoice ? program.invoice : "None"}
+            </h4>
+          )}
+        </div>
+        <div>
+          <h2 className="font-bold text-3xl">Invoice Paid: </h2>
+          {editMode ? (
+            <input
+              type="checkbox"
+              id="editInvoicePaid"
+              defaultChecked={program.invoicePaid}
+            />
+          ) : (
+            <h4 className=" bg-emerald-50 px-3 py-2 rounded-lg w-fit">
+              {program.invoicePaid.toString()}
+            </h4>
+          )}
+        </div>
+        <div>
+          <h2 className="font-bold text-3xl">Number of Classes: </h2>
+          {editMode ? (
+            <input
+              type="number"
+              id="editClasses"
+              defaultValue={program.classes}
+            />
+          ) : (
+            <h4 className=" bg-emerald-50 px-3 py-2 rounded-lg w-fit">
+              {program.classes ? program.classes : "0"}
+            </h4>
+          )}
+        </div>
+        <div>
+          <h2 className="font-bold text-3xl">Number of Students: </h2>
+          {editMode ? (
+            <input
+              type="number"
+              id="editStudents"
+              defaultValue={program.students}
+            />
+          ) : (
+            <h4 className=" bg-emerald-50 px-3 py-2 rounded-lg w-fit">
+              {program.students ? program.students : "0"}
+            </h4>
+          )}
+        </div>
+        <div>
+          <h2 className="font-bold text-3xl">Number of Weeks: </h2>
+          {editMode ? (
+            <input type="number" id="editWeeks" defaultValue={program.weeks} />
+          ) : (
+            <h4 className=" bg-emerald-50 px-3 py-2 rounded-lg w-fit">
+              {program.weeks ? program.weeks : "0"}
+            </h4>
+          )}
+        </div>
+        <div>
+          <h2 className="font-bold text-3xl">Assigned Staff: </h2>
+          {editMode ? (
+            <input
+              type="text"
+              id="editStaff"
+              placeholder="Staff1, Staff2,"
+              defaultValue={program.staff}
+            />
+          ) : (
+            <h4 className=" bg-emerald-50 px-3 py-2 rounded-lg w-fit">
+              {program.staff ? program.staff : "None"}
+            </h4>
+          )}
+        </div>
+        <div>
+          <h2 className="font-bold text-3xl">Price Per Student: </h2>
+          {editMode ? (
+            <input
+              type="number"
+              id="editPrice"
+              defaultValue={program.pricing}
+            />
+          ) : (
+            <h4 className=" bg-emerald-50 px-3 py-2 rounded-lg w-fit">
+              ${program.pricing}
+            </h4>
+          )}
+        </div>
+        <div>
+          <h2 className="font-bold text-3xl">Price Per Month: </h2>
+          <h4 className=" bg-emerald-50 px-3 py-2 rounded-lg w-fit">
+            ${program.pricing * program.students}
+          </h4>
+        </div>
+        <div>
+          <h2 className="font-bold text-3xl">Program Total: </h2>
+          <h4 className=" bg-emerald-50 px-3 py-2 rounded-lg w-fit">
+            ${program.pricing * program.students * program.weeks}
+          </h4>
+        </div>
       </div>
-      Active:{" "}
-      {editMode ? (
-        <input
-          type="checkbox"
-          id="editStatus"
-          defaultChecked={!!program.status}
-        />
-      ) : (
-        program.status.toString()
-      )}
-      <p>
-        {editMode ? (
-          <textarea
-            id="editDescription"
-            style={{ height: 100, width: 300 }}
-            defaultValue={program.description}
-            placeholder="Description of program"
-          />
-        ) : (
-          program.description
-        )}
-      </p>
-      <p>
-        Invoice due date:{" "}
-        {editMode ? (
-          <input
-            type="date"
-            id="editInvoiceDate"
-            // defaultValue of due date is the programs due date converted from a date object to a string
-            defaultValue={
-              program.invoice
-                ? new Date(program.invoice).toISOString().split("T")[0]
-                : ""
-            }
-          />
-        ) : (
-          program.invoice
-        )}
-      </p>
-      <p>
-        Invoice paid:{" "}
-        {editMode ? (
-          <input
-            type="checkbox"
-            id="editInvoicePaid"
-            defaultChecked={program.invoicePaid}
-          />
-        ) : (
-          program.invoicePaid.toString()
-        )}
-      </p>
-      <p>
-        Number of classes:{" "}
-        {editMode ? (
-          <input
-            type="number"
-            id="editClasses"
-            defaultValue={program.classes}
-          />
-        ) : (
-          program.classes
-        )}
-      </p>
-      <p>
-        Number of students:{" "}
-        {editMode ? (
-          <input
-            type="number"
-            id="editStudents"
-            defaultValue={program.students}
-          />
-        ) : (
-          program.students
-        )}
-      </p>
-      <p>
-        Price Per Student:{" "}
-        {editMode ? (
-          <input type="number" id="editPrice" defaultValue={program.pricing} />
-        ) : (
-          program.pricing
-        )}
-      </p>
-      <p>Price Per month: {program.pricing * program.students}</p>
-      <p>Program Total: {program.pricing * program.students * program.weeks}</p>
-      <p>
-        Number of weeks in the program:{" "}
-        {editMode ? (
-          <input type="number" id="editWeeks" defaultValue={program.weeks} />
-        ) : (
-          program.weeks
-        )}
-      </p>
-      <p>
-        Assigned Staff:{" "}
-        {editMode ? (
-          <input
-            type="text"
-            id="editStaff"
-            placeholder="Staff1, Staff2,"
-            defaultValue={program.staff}
-          />
-        ) : (
-          program.staff
-        )}
-      </p>
-      <button type="button" onClick={() => dispatch(removeProgram(program.id))}>
+      <button
+        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+        type="button"
+        onClick={() => dispatch(removeProgram(program.id))}
+      >
         Delete
       </button>
       {/* Button to edit the current Program */}
-      <button type="button" onClick={() => setEditMode(!editMode)}>
+      <button className="" type="button" onClick={() => setEditMode(!editMode)}>
         {editMode ? "Cancel" : "Edit"}
       </button>
       {editMode ? (
-        <button type="button" onClick={saveChanges}>
+        <button
+          className="savebtn bg-green-400"
+          type="button"
+          onClick={saveChanges}
+        >
           Save
         </button>
       ) : null}
