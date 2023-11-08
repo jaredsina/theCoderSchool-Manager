f69e3d49d65f716df39da14718e76560733eb8f4
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-11-08 11:27:20 -0500

    fix: Change the way files are loading to match the new redux state

--- client/src/components/FileList.js
+++ client/src/components/FileList.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import PropTypes from "prop-types";
 import { deleteFile } from "../reducers/fileReducer";
 
-const FileList = () => {
+const FileList = ({ id }) => {
   const dispatch = useDispatch();
   const files = useSelector((state) => state.files);
-  const filesList = files.map((file) => (
-    <li key={file.id}>
-      <a href={file.url} target="_blank" rel="noreferrer">
-        {file.filename}
-      </a>
-      <button type="button" onClick={() => dispatch(deleteFile(file.id))}>
-        Delete
-      </button>
-    </li>
-  ));
+  const filesList = files
+    .filter((f) => {
+      return f.programId === id;
+    })
+    .map((file) => (
+      <li key={file.id}>
+        <a href={file.url} target="_blank" rel="noreferrer">
+          {file.filename}
+        </a>
+        <button type="button" onClick={() => dispatch(deleteFile(file.id))}>
+          Delete
+        </button>
+      </li>
+    ));
   return (
     <div>
       <h4 className="font-bold text-3xl">File List</h4>
@@ -24,3 +29,7 @@ const FileList = () => {
 };
 
 export default FileList;
+
+FileList.propTypes = {
+  id: PropTypes.string.isRequired,
+};
