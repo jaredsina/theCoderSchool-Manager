df7755aa6a9afef94b659927490c15226a02ff21
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-12-15 10:58:43 -0500

    feat: Add view button and styling to task list

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -7,9 +7,27 @@ const TaskList = () => {
   const dispatch = useDispatch();
   const tasks = useSelector((state) => state.tasks);
   const taskList = tasks.map((task) => (
-    <li key={task.id}>
-      <Link to={`/dashboard/${task.id}`}>{task.name}</Link>
-      <button type="button" onClick={() => dispatch(deleteTask(task.id))}>
+    <li
+      key={task.id}
+      className="grid grid-cols-4 py-2 border-b border-emerald-950"
+    >
+      <Link
+        to={`/dashboard/${task.id}`}
+        className="whitespace-nowrap overflow-auto col-span-2"
+      >
+        {task.name}
+      </Link>
+      <Link
+        to={`/dashboard/${task.id}`}
+        className="bg-green-400 hover:bg-green-500 text-white font-bold mx-4 text-center rounded hover:scale-105 transition-all col-start-3"
+      >
+        View
+      </Link>
+      <button
+        type="button"
+        onClick={() => dispatch(deleteTask(task.id))}
+        className="bg-red-600 hover:bg-red-700 text-white font-bold px-1 rounded hover:scale-105 transition-all col-start-4 mx-4"
+      >
         Delete
       </button>
     </li>
@@ -17,6 +35,9 @@ const TaskList = () => {
   return (
     <div>
       <h4 className="font-bold text-3xl">Task List</h4>
+      <div className="grid grid-cols-4 border-b border-emerald-950 mt-4">
+        <p className="">Task Name</p>
+      </div>
       <ul>{taskList}</ul>
     </div>
   );
