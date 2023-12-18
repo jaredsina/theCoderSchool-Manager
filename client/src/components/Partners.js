caf0be98a56fdc415412cfaccd101682f0d4383a
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-12-18 10:15:12 -0500

    feat: Add styling to partners page

--- client/src/components/Partners.js
+++ client/src/components/Partners.js
@@ -12,26 +12,35 @@ const Partners = () => {
       <div
         key={partner.id}
         onClick={() => navigate(`/dashboard/${partner.id}`)}
+        className="bg-emerald-50 shadow-md rounded-lg p-4 hover:scale-110 hover:shadow-lg transition-all cursor-pointer"
       >
         <p>{partner.name}</p>
+        <p>{partner.email}</p>
+        <p>{partner.phone}</p>
       </div>
     );
   });
   const modalRef = useRef();
   return (
-    <div>
-      <h1>Partners</h1>
-      <Modal ref={modalRef} header="New Partner Form">
-        <PartnerForm closeModal={() => modalRef.current.close()} />
-      </Modal>
-      <button
-        type="button"
-        onClick={() => modalRef.current.openModal()}
-        className="bg-emerald-950 text-white font-bold px-4 py-2 rounded hover:scale-105 transition-all"
-      >
-        Create Partner
-      </button>
-      {partnerCards}
+    <div className="lg:px-8">
+      <div className="flex justify-end">
+        <Modal ref={modalRef} header="New Partner Form">
+          <PartnerForm closeModal={() => modalRef.current.close()} />
+        </Modal>
+        <button
+          type="button"
+          onClick={() => modalRef.current.openModal()}
+          className="bg-emerald-950 text-white font-bold p-2 rounded-lg hover:scale-105 transition-all"
+        >
+          Create Partner
+        </button>
+      </div>
+      <div className="shadow-lg rounded-lg p-4 border border-gray-50 mt-4">
+        <h2 className="font-bold mb-4">Partners</h2>
+        <div className="p-2 bg-transparent overflow-y-auto overflow-x-hidden grid gap-4 grid-cols-2 lg:grid-cols-4">
+          {partnerCards}
+        </div>
+      </div>
     </div>
   );
 };
