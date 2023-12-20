42f9ca90d97ae8c17561e3eea9e35f6033142463
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-12-20 10:40:07 -0500

    fix: Fix styling on buttons

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -11,6 +11,7 @@ const Tasks = () => {
   useEffect(() => {
     dispatch(initializeTasks());
   }, [dispatch]);
+
   const tasks = useSelector((state) => state.tasks);
 
   const tasksList = tasks.map((task) => {
@@ -18,33 +19,31 @@ const Tasks = () => {
       <div
         key={task.id}
         onClick={() => navigate(`/dashboard/${task.id}`)}
-        className="bg-emerald-50 p-4 hover:bg-emerald-300 transition-all cursor-pointer grid grid-cols-5"
+        className="bg-emerald-50 p-4 hover:bg-emerald-300 transition-all cursor-pointer grid grid-cols-7"
       >
         <p>{task.name}</p>
         <p className="col-span-2">{task.description}</p>
-        <p>{task.dueDate}</p>
-        <div>
-          <Link
-            to={`/dashboard/${task.id}`}
-            className="col-start-3 bg-green-400 hover:bg-green-500 text-white font-bold mx-4 text-center rounded hover:scale-105 transition-all cursor-pointer"
-          >
-            View
-          </Link>
-          <button
-            type="button"
-            className="bg-red-600 hover:bg-red-700 text-white font-bold px-1 mx-4 rounded hover:scale-105 transition-all col-start-4"
-            onClick={() => dispatch(deleteTask(task.id))}
-          >
-            Delete
-          </button>
-        </div>
+        <p className="col-span-2">{task.dueDate}</p>
+        <Link
+          to={`/dashboard/${task.id}`}
+          className=" bg-green-400 hover:bg-green-500 text-white font-bold rounded hover:scale-105 transition-all p-1 mx-4 text-center"
+        >
+          View
+        </Link>
+        <button
+          type="button"
+          className="bg-red-600 hover:bg-red-700 text-white font-bold rounded hover:scale-105 transition-all p-1 mx-4"
+          onClick={() => dispatch(deleteTask(task.id))}
+        >
+          Delete
+        </button>
       </div>
     );
   });
   return (
     <div className="lg:px-8">
       <h4 className="font-bold text-3xl">Task List</h4>
-      <div className="grid grid-cols-5 border-b border-emerald-950 mt-4">
+      <div className="grid grid-cols-7 border-b border-emerald-950 mt-4">
         <p className="">Task Name</p>
         <p className=" col-span-2">Description</p>
         <p>Due Date</p>
