df9751f8e61d9a231ddd5b4d15d2abf75e30cce1
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2023-12-22 11:51:56 -0500

    feat: Add styling to files cards

--- client/src/components/Files.js
+++ client/src/components/Files.js
@@ -13,18 +13,31 @@ const Files = () => {
   const files = useSelector((state) => state.files);
   const filesList = files.map((file) => {
     return (
-      <div key={file.id} className="">
-        <img src={file.url} alt={file.filename} className="" />
-        <a href={file.url} target="_blank" rel="noreferrer">
+      <div
+        key={file.id}
+        className=" bg-emerald-50 rounded-md p-2 hover:-translate-y-2 transition-all"
+      >
+        <a
+          href={file.url}
+          target="_blank"
+          rel="noreferrer"
+          className="whitespace-nowrap overflow-hidden overflow-ellipsis flex cursor-pointer"
+          title={file.filename}
+        >
           {file.filename}
         </a>
+        <img
+          src={file.url}
+          alt={file.filename}
+          className="mt-2 h-40 w-full object-cover rounded-md"
+        />
       </div>
     );
   });
   return (
     <div className="lg:px-8">
       <h4 className="font-bold text-3xl">Files</h4>
-      <div className="grid grid-cols-3">{filesList}</div>
+      <div className="grid md:grid-cols-6 gap-4 mt-4">{filesList}</div>
     </div>
   );
 };
