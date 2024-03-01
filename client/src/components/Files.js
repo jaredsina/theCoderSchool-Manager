812d76cea6d311195aa8de0fe750ae7fd1d5e726
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-03-01 11:17:31 -0500

    feat: Create delete file button for all files in the database

--- client/src/components/Files.js
+++ client/src/components/Files.js
@@ -1,9 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { initializeFilesWithUrls } from "../reducers/fileReducer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { initializeFilesWithUrls, deleteFile } from "../reducers/fileReducer";
 
 const Files = () => {
   const dispatch = useDispatch();
+  const deleteFileHandler = (id) => {
+    const confirm = window.confirm(
+      "Are you sure you want to delete this file?",
+    );
+    if (confirm) {
+      dispatch(deleteFile(id));
+    }
+  };
   const user = useSelector((state) => state.user);
   useEffect(() => {
     if (user) {
@@ -31,6 +41,21 @@ const Files = () => {
           alt={file.filename}
           className="mt-2 h-40 w-full object-cover rounded-md"
         />
+        <button
+          className=" hover:bg-red-200 w-8 h-8 mx-auto mt-2 rounded-md cursor-pointer flex justify-center items-center"
+          onClick={() => deleteFileHandler(file.id)}
+          type="button"
+        >
+          <FontAwesomeIcon
+            icon={faTrashCan}
+            style={{
+              width: "100%",
+              height: "1.5em",
+              color: "rgb(239,68,68)",
+            }}
+            className=" "
+          />
+        </button>
       </div>
     );
   });
