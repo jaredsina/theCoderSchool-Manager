fa4f794687c126958811d4d960ff1f1bd03ed8c3
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-03-21 11:47:20 -0400

    fix: Fix lint errors

--- client/src/utils/test-utils.js
+++ client/src/utils/test-utils.js
@@ -1,12 +1,15 @@
 import React from "react";
+
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { render } from "@testing-library/react";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import PropTypes from "prop-types";
 import authReducer from "../reducers/authReducer";
 import notificationReducer from "../reducers/notificationReducer";
 
-export function renderWithProviders(
+function renderWithProviders(
   ui,
   {
     preloadedState = {},
@@ -25,7 +28,11 @@ export function renderWithProviders(
       </BrowserRouter>
     );
   };
-
+  Wrapper.propTypes = {
+    children: PropTypes.elementType.isRequired,
+  };
   // Return an object with the store and all of RTL's query functions
   return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
 }
+
+export default renderWithProviders;
