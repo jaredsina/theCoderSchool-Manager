fd622c93d231806293acc5dbfe1f750aba7d2447
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-03-27 11:24:56 -0400

    fix: Fix decoded user

--- backend/middleware/userExtractor.js
+++ backend/middleware/userExtractor.js
@@ -1,9 +1,8 @@
 const User = require("../models/user");
 
 // middleware to extract user id from token
-const userExtractor = (request, response, next) => {
-  request.user = User.findById(request.decodedToken);
-
+const userExtractor = async (request, response, next) => {
+  request.user = await User.findById(request.decodedToken);
   next();
 };
 
