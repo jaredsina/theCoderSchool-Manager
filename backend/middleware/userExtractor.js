ac48b753189c850546947c3725528084d297b74e
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-03-28 12:25:56 -0400

    fix: Fix decodedToken having sensitive data

--- backend/middleware/userExtractor.js
+++ backend/middleware/userExtractor.js
@@ -2,7 +2,11 @@ const User = require("../models/user");
 
 // middleware to extract user id from token
 const userExtractor = async (request, response, next) => {
-  request.user = await User.findById(request.decodedToken);
+  request.user = await User.findById(request.decodedToken, {
+    passwordHash: 0,
+    _id: 0,
+    __v: 0,
+  });
   next();
 };
 
